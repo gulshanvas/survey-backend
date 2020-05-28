fd43f38689efd541102ff06aafa8b5863b0d4fcb
gulshanvas <gulshanslvasnani@gmail.com>
2020-05-28 22:40:41 +0530

    Added introspection and playground option in apollo

--- apollo/index.js
+++ apollo/index.js
@@ -1,8 +1,11 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
-// syntax error near unexpected token `('
-const server = new ApolloServer({ typeDefs });
 
+const server = new ApolloServer({
+  typeDefs,
+  introspection: true,
+  playground: true,
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
