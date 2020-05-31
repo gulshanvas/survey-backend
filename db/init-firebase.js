9bdeb75cc13760c5996bb9a25d98b78323eeef56
gulshanvas <gulshanslvasnani@gmail.com>
2020-05-31 21:50:08 +0530

    Added environment variables

---
+++ db/init-firebase.js
@@ -0,0 +1,19 @@
+const firebase = require('firebase');
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+module.exports = db;
--- env_variables.sh
+++ env_variables.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
-export JWT_SECRET_KEY='coronafreeworld'
+export JWT_SECRET_KEY=''
 
-export PASSWORD_SALT='$2a$10$WRySE6mlfYnyHPaBI.W57u' 
+export PASSWORD_SALT='' 
+
+
+# Firebase config variables
+
+export FB_API_KEY=''
+
+export FB_AUTH_DOMAIN=''
+
+export FB_DATABASE_URL=''
+
+export FB_PROJECT_ID=''
+
+export FB_STORAGE_BUCKET=''
+
+export FB_MESSAGING_SENDER_ID=''
+
+export FB_APP_ID=''
+
+export FB_MEASUREMENT_ID=''
